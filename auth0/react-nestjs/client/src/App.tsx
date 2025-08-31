5b1761cc62fda82e2a8f63349609a856055dca25
scintill-a <giangigantoca@gmail.com>
2025-08-31 21:45:32 +0800

    auth0 client provider and login/signup page

--- auth0/react-nestjs/client/src/App.tsx
+++ auth0/react-nestjs/client/src/App.tsx
@@ -1,5 +1,16 @@
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen flex items-center flex-col justify-center bg-[#1e1e1e]">
+      <h1 className="font-bold mb-2 text-center text-white">
+        Auth0 - [React-NestJS]
+      </h1>
+      <div className="bg-white rounded-xl shadow-md px-12 py-8 flex flex-col items-center">
+        <button className="px-8 py-3 rounded-lg bg-[#1e1e1e] text-white font-medium text-lg hover:bg-[#181818] text-white transition">
+          Login | Signup
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- auth0/react-nestjs/client/src/main.tsx
+++ auth0/react-nestjs/client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <Auth0Provider
+    domain={import.meta.env.VITE_AUTH0_CLIENT_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: import.meta.env.VITE_AUTH0_API_AUDIENCE || "<API_AUDIENCE>", // API audience
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>
+);
